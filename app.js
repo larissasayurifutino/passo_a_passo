8b347ee0300732cf790e5f89026c38b4eccbf155
Larissa Sayuri Futino Castro dos Santos <44413674+larissasayurifutino@users.noreply.github.com>
2022-05-29 13:09:12 -0300

    Add js code.

---
+++ app.js
@@ -0,0 +1,30 @@
+// 1. criar uma função para verificar 
+// se o gatilho chegou ao topo da página
+function escutaScroll(event){
+    // A função deve...
+    // Pegar a lista de gatilhos
+    let gatilhos = document.querySelectorAll(".gatilhos > div");
+
+    // A função deve...
+    // Fazer loop pela lista de gatilhos
+    for(let gatilho of gatilhos){
+        // Para cada um deles, pegar a posição atual
+        let posicao = gatilho.getBoundingClientRect();
+        // Para cada um deles, pegar o valor da propriedade data-alvo
+        let alvo = gatilho.dataset.alvo;
+        // Selecionar o elemento reference a este alvo
+        let passo = document.querySelector('.'+alvo);
+        // Verificar se o gatilho está acima do topo da página
+        if(posicao.top <= 0  && posicao.top > -posicao.height/2){
+            // se sim, adiciona a classe que exibe o gráfico
+            passo.classList.add('passo-ativo');
+        }else{
+            // caso contrário, retire a classe
+            passo.classList.remove('passo-ativo');
+        }
+    }
+}
+
+// 2. pedir ao navegador para escutar 
+// o evento de rolagem da página
+window.addEventListener('scroll', escutaScroll);
